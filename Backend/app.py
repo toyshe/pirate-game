55ff63850ac3c6528a868450e1a2ee49eda98d28
toyshe <toyshehossain@outlook.com>
2024-03-01 18:24:30 +0000

    adding new line in run() for deploy

--- Backend/app.py
+++ Backend/app.py
@@ -126,4 +126,4 @@ def disconnect():
     print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
